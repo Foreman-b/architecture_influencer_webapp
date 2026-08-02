5992e0d5c16b3b659ee65ae1a5a4193f539ba34e
Foreman-b <hamzat4life@gmail.com>
2026-08-02 21:13:23 +0100

    add build.sh file

---
+++ .build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# Exit immediately if a command exits with a non-zero status
+set -o errexit
+
+# Install project dependencies
+pip install --upgrade pip
+pip install -r requirements.txt
+
+# Collect static files for production
+python manage.py collectstatic --no-input
+
+# Run database migrations
+python manage.py migrate
