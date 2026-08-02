8ffdfed67bc31e615c8df141867502ffe09e2bb8
Abdulfatai A. Hamsat <85897185+Foreman-b@users.noreply.github.com>
2026-08-02 21:46:46 +0100

    Set DEBUG to False in settings.py
    Changed DEBUG setting to False for production.

--- architecture_influencer/settings.py
+++ architecture_influencer/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fr17r&koz2$^9h3ti9o8uccxer%!^ayp^4t*_+wr4cg7y2o(!e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["127.0.0.1", "localhost", "architectureinfluencer.onrender.com", "https://architectureinfluencer.onrender.com"]
 
